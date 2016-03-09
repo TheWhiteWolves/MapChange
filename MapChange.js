08acb4c1d50f55acef38c87164b869c0ffc80f17
Jonathan Smith <jonathan.smith@whitestarco.com>
2016-03-09 12:54:58 +0000

    Finish making script use variable parameters
    Fixes #2 - Allowing the use of spaces in names etc and added in some comments to first half of the script

--- MapChange.js
+++ MapChange.js
@@ -7,12 +7,12 @@
 // Done * !mc refresh                           - Refreshes the public and private maps.
 // Done * !mc rejoin                            - Removes the senders's playerspecificpages entry and moves 
 //                                                them back to the global players tab.
-//      * !mc rejoin [player name]              - Removes the provided player's playerspecificpages entry and  
+// Done * !mc rejoin [player name]              - Removes the provided player's playerspecificpages entry and  
 //                                                moves them back to the global players tab.
-// Done * !mc move [map name]                   - Moves the sender to the map with the provided name.
+// Done * !mc move [map name]                   - Moves te sender to the map with the provided name.
 //                                                (GM required for private maps)
-// Done * !mc moveplayer [player] [map name]    - Moves the player with the provided name to the map with
-//                                                the provided name. (GM required for private maps)
+// Done * !mc move [player name] [map name]     - Moves the sender with the provided name to the map with the 
+//                                                provided name. (GM required for private maps)
 // Done * !mc moveall [map name]                - Empties playerspecificpages and moves all players to the
 //                                                map with the provided name. (GM Only)
 //      * !mc help                              - Display help on how to use the script.
@@ -20,9 +20,11 @@
 var MapChange = MapChange || (function() {
     'use strict';
     // Config
+    // Set to true to use built in debug statements
+    var debug = true,
     // When true places the pages with name containing the marker into the public list.
     // Use this if you want maps to be private by default instead of public by default.
-    var invertedMarker = false,
+    invertedMarker = false,
     // The marker used to decide what is placed in the private map.
     marker = "[GM]",
     // These are maps that players are able to move to using the commands.
@@ -36,8 +38,9 @@ var MapChange = MapChange || (function() {
         
         // Loop through the pages adding them to their relevent maps.
         for (var key in pages) {
-            // Grab the name and id for the current page.
+            // Get the name of the page that is current being processed.
             var name = pages[key].get("name");
+            // Get the id of the page that is current being processed.
             var id = pages[key].get("_id");
             
             // Check if the name of the page contains the marker.
@@ -56,10 +59,12 @@ var MapChange = MapChange || (function() {
         }
         
         // Debug
-        log("Public:");
-        log(publicMaps);
-        log("Private:");
-        log(privateMaps);
+        if (debug) {
+            log("Public:");
+            log(publicMaps);
+            log("Private:");
+            log(privateMaps);
+        }
     },
     
     handleInput = function(msg) {
@@ -75,72 +80,95 @@ var MapChange = MapChange || (function() {
         // Parse the remaining aruments to get any parameters passed in.
         var params = parseParameters(args);
         
-        log(commands);
-        log(params);
-        
         // Check the lower cased version of the message to see if it contains the call for
         // this script to run, if it doesn't then return.
         switch (commands.shift().toLowerCase()) {
             case "!mapchange":
             case "!mc":
+                // Check to see if any further commands were passed in and process them, else
+                // show the help test on how to use the script.
                 if (commands.length > 0) {
+                    // Process the remaining commands with the passed in paramters.
                     processCommands(msg, commands, params);
                 }
                 else {
+                    // Show the sender the script help message.
                     showHelp(msg);
                 }
                 break;
             default:
+                // If we reached here it means that the call to the api was not meant for us.
                 return;
         }
     },
     
     parseCommands = function(args) {
+        // Split the arguments by spaces and return the array containing them all.
         return args.split(/\s+/);
     },
     
     parseParameters = function(args) {
+        // Declare a new object to hold the parameters.
         var params = {};
+        // Loop through all the passed in arguments and construct them in into the parameters.
         for (var param in args) {
+            // Split the parameter down by spaces and temporarily store it.
             var tmp = args[param].split(/\s+/);
+            // Take the first element in tmp and use it as the parameter name and reassemble the
+            // remaining elements and replace the commas with spaces for the parameter value.
             params[tmp.shift()] = tmp.join().replace(/,/g, " ");
         }
+        // Return the constructed object of parameters.
         return params;
     },
     
     processCommands = function(msg, commands, params) {
+        // Take the command and decide what function to run.
         switch (commands.shift().toLowerCase()) {
             case "help":
-                showHelp();
+                // Send the help text to the player who sent the message.
+                showHelp(msg);
                 break;
             case "refresh":
+                // Refresh the public and private maps to pull in any changes made since the script
+                // was last started.
                 refresh();
                 break;
             case "move":
+                // Check to see if the sender has provided and target map for the move, if they
+                // haven't then send them a chat message to tell them it is missing.
                 if(params.hasOwnProperty("target")) {
+                    // Check to see if the sender has provided a player to be moved, if they 
+                    // haven't then user the id of the sender.
                     if (params.hasOwnProperty("player")) {
+                        // Run the player move twice, currently there is strange behaviour happening
+                        // that requires a second run to update the instance.
                         move(msg, getPlayerIdFromDisplayName(params.player), params.target);
                         move(msg, getPlayerIdFromDisplayName(params.player), params.target);
                     }
                     else {
+                        // Run the player move twice, currently there is strange behaviour happening
+                        // that requires a second run to update the instance.
                         move(msg, msg.playerid, params.target);
                         move(msg, msg.playerid, params.target);
                     }
                 }
                 else {
+                    // Send a chat message to tell teh sender that they missed the target map parameter.
                     sendChat("MapChange", "/w " + msg.who + " Target map parameter missing, use !mc help to see how to use this script.");
                 }
                 break;
             case "rejoin":
-                if (args.length > 0) {
-                    rejoin(getPlayerIdFromDisplayName(args.shift()));
+                if (params.hasOwnProperty("player")) {
+                    rejoin(getPlayerIdFromDisplayName(params.player));
                 }
                 else {
                     rejoin(msg.playerid);
                 }
                 break;
             case "moveall":
-                moveall(msg, args.join().replace(/,/g, " "));
+                moveall(msg, params.target);
+                moveall(msg, params.target);
                 break;
             default:
                 showHelp(msg);
@@ -190,7 +218,7 @@ var MapChange = MapChange || (function() {
             playerPages[sender] = publicMaps[target];
         }
         else if (target in privateMaps) {
-            if(playerIsGM(sender)) {
+            if(playerIsGM(msg.playerid)) {
                 // Move player.
                 if(sender in playerPages) {
                     delete playerPages[sender];
@@ -209,7 +237,7 @@ var MapChange = MapChange || (function() {
 
     rejoin = function(sender) {
         var playerPages = Campaign().get("playerspecificpages");
-        if(sender in playerPages) {
+        if (sender in playerPages) {
             delete playerPages[sender];
             Campaign().set("playerspecificpages", false);
         }
@@ -217,7 +245,6 @@ var MapChange = MapChange || (function() {
     },
 
     moveall = function(msg, target) {
-        log(playerIsGM(msg.playerid));
         if (playerIsGM(msg.playerid)) {
             var bookmarkPage = Campaign().get("playerpageid");
             if (target in publicMaps) {
