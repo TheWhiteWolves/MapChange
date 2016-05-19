3bf5a23c7b3d327ffdd4e7fcbbdf049604e943b7
Jonathan Smith <jonathan.smith@whitestarco.com>
2016-05-19 16:36:05 +0100

    Basics of the script.js file for the script repo

---
+++ script.js
@@ -0,0 +1,39 @@
+{
+  "name": "MapChange",
+  "script": "MapChange.js",
+  "version": "1.0",
+  "description": "Provides a way for users to move between maps in a campaign withput the GM needing to move them.", 
+  "authors": "TheWhiteWolves",
+  "roll20userid": "1043",
+  "patreon": "",
+  "useroptions": [
+    {
+      "name": "Debug Mode",
+      "type": "checkbox",
+      "description": "",
+      "value": "debug"
+    },
+    {
+      "name": "GM Notification",
+      "type": "checkbox",
+      "description": "",
+      "value": "gmNotify"
+    },
+    {
+      "name": "Marker",
+      "type": "text",
+      "default": "[GM]",
+      "description": "",
+    },
+    {
+      "name": "Inverted Marker",
+      "type": "checkbox",
+      "description": "",
+      "value": "invertedMarker"
+    },
+  ],
+  "dependencies": [],
+  "modifies": {},
+  "conflicts": [],
+  "previousversions": []
+}
